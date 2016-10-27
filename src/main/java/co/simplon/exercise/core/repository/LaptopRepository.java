f3a01221ca276cf77435ed1b6f154479b4ca1852
Salah <amberandoud@gmail.com>
2016-10-27 23:50:06 +0200

    add repositories for resources

---
+++ src/main/java/co/simplon/exercise/core/repository/LaptopRepository.java
@@ -0,0 +1,12 @@
+package co.simplon.exercise.core.repository;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.simplon.exercise.core.model.Laptop;
+
+@Resource
+public interface LaptopRepository extends JpaRepository<Laptop, Integer> {
+
+}
