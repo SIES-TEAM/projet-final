6611457ed58401db8296fe5661cb177fe90e42f0
Salah <amberandoud@gmail.com>
2016-10-29 05:49:08 +0200

    fix bug due to format of date

--- src/main/java/co/simplon/exercise/web/ReservationController.java
+++ src/main/java/co/simplon/exercise/web/ReservationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import co.simplon.exercise.core.model.Reservation;
 import co.simplon.exercise.core.service.ReservationService;
@@ -47,14 +48,17 @@ public class ReservationController {
 	
 	@RequestMapping(path = "/add")
 	public ModelAndView addReservation(@RequestParam int userId,
-			                           @RequestParam @DateTimeFormat(pattern = "ddMMyyyy")Date creationDate,
-			                           @RequestParam @DateTimeFormat(pattern = "ddMMyyyy")Date dateBegin,
-			                           @RequestParam @DateTimeFormat(pattern = "ddMMyyyy")Date dateEnd,
+			                           @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")Date creationDate,
+			                           @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")Date dateBegin,
+			                           @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")Date dateEnd,
 			                           ModelMap model
+			                           
 			                          )
 	{
 		//if(result.hasError)
 		reservationService.addOrUpdateReservation(new Reservation(userId, creationDate, dateBegin, dateEnd));
+		
+		//redirectAttribute.addFlashAttribute("success", "Réservation ajoutée avec succès !");
 		return new ModelAndView("redirect:/reservations/formAdd");
 		
 	}
