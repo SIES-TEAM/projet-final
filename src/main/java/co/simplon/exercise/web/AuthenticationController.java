8c2a932e7bdc28ed7f0bbe20d9417ec2de4bcbac
Salah <amberandoud@gmail.com>
2016-11-08 10:45:02 +0100

    add authentication controller

--- src/main/java/co/simplon/exercise/web/AuthenticationController.java
+++ src/main/java/co/simplon/exercise/web/AuthenticationController.java
@@ -1,7 +1,38 @@
-package co.simplon.exercise.web;
-
-/**
- * Created by salah on 08/11/16.
- */
-public class AuthenticationController {
-}
+//package co.simplon.exercise.web;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//public class AuthenticationController {
+//
+//    @RequestMapping(path = "/signin")
+//    public ModelAndView signin(ModelMap model) {
+//        return new ModelAndView("jsp/signin");
+//    }
+//
+//    @RequestMapping(path = "/loginForm")
+//    public ModelAndView loginForm(ModelMap model) {
+//        return new ModelAndView("jsp/loginForm");
+//    }
+//
+//    @RequestMapping(value = "/logout")
+//    public ModelAndView logoutPage(HttpServletRequest request, HttpServletResponse response) {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (auth != null) {
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//        }
+//        return new ModelAndView("redirect:/");
+//    }
+//
+//    @RequestMapping(value = "/accessDenied")
+//    public String accessDeniedPage(ModelMap model) {
+//        return "jsp/accessDenied";
+//}
