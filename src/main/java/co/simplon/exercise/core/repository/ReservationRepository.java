091a557926198098f247c8f0409b4d6ead3df3b5
Salah <amberandoud@gmail.com>
2016-12-10 18:57:44 +0100

    change the query for the perform result

--- src/main/java/co/simplon/exercise/core/repository/ReservationRepository.java
+++ src/main/java/co/simplon/exercise/core/repository/ReservationRepository.java
@@ -21,17 +21,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Intege
     List<Laptop> findLaptopsByBookingDate(@Param("bookingDate") LocalDate bookingDate,
                                    @Param("startTime") LocalTime startTime,
                                    @Param("endTime") LocalTime endTime);
+
     @Query("SELECT r.classroom from Reservation r where r.bookingDate= :bookingDate and r.startTime = :startTime and r.endTime= :endTime")
     List<Classroom> findRoomsByBookingDate(@Param("bookingDate") LocalDate bookingDate,
                                            @Param("startTime") LocalTime startTime,
                                            @Param("endTime") LocalTime endTime);
-
-    @Query("SELECT Laptop FROM Reservation r WHERE  r.bookingDate <> :bookingDate " +
-            "or( r.bookingDate = :bookingDate and r.startTime not between :startTime and :endTime )  " +
-            "or r.bookingDate = :bookingDate and r.endTime not between  :startTime and :endTime" )
-    List<Laptop> findLaptopsByBookingDateAndHour(@Param("bookingDate") LocalDate bookingDate,
-                                          @Param("startTime") LocalTime startTime,
-                                          @Param("endTime") LocalTime endTime);
 
 
 }
