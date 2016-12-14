69eaabe83b610df4c2e1281c6ddc0fa9f3b55ad6
Salah <amberandoud@gmail.com>
2016-12-14 19:08:04 +0100

    bug due to inexisting method name

---
+++ src/test/java/co/simplon/exercise/core/service/LaptopServiceTest.java
@@ -0,0 +1,16 @@
+package co.simplon.exercise.core.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by salah on 14/12/16.
+ */
+public class LaptopServiceTest {
+    @Test
+    public void getAvailableLaptops() throws Exception {
+
+    }
+
+}
