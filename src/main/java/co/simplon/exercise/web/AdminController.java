70f920194ef53bdd07e680c14aa2d9dc3975d539
Salah <amberandoud@gmail.com>
2016-12-14 18:52:22 +0100

    admin interface to be continued

--- src/main/java/co/simplon/exercise/web/AdminController.java
+++ src/main/java/co/simplon/exercise/web/AdminController.java
@@ -8,12 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path = "/admin")
+@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
     private ReservationService reservationService;
 
+    public ModelAndView dashboard(ModelMap model) {
+        return new ModelAndView("admin/dashboard");
+    }
+    @RequestMapping(path = "/booking/historic")
     public ModelAndView showAllReservations(ModelMap model) {
         model.addAttribute("reservations", reservationService.getAll());
         return new ModelAndView("reservation/reservations");
---
+++ src/main/webapp/WEB-INF/view/admin/dashboard.jsp
@@ -0,0 +1,13 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Admin dashboard</title>
+</head>
+<body>
+    <h1 class="page-header">Dashboard</h1>
+
+    <p> To be continued . . .</p>
+
+</body>
+</html>
